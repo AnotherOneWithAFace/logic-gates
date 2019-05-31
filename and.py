08cddc8e46707d5c6792456fe6080ee52ff2d580
Schloopy <schloopy@protonmail.ch>
2019-05-31 10:40:12 +0100

    still working on it, comes up with a logical error if you enter anything other than 1 or 0, at least for now

--- and.py
+++ and.py
@@ -5,12 +5,16 @@ def main():
     y = input()
     if x == "0":
         bool1 = False
-    else:
+    elif x == "1":
         bool1 = True
+    else:
+        x = None
     if y == "1":
         bool2 = True
-    else:
+    elif y == "0":
         bool2 = False
+    else:
+        y = None
     
     if bool2 and bool1 == True:
         print("Your answer is: 1")
@@ -18,6 +22,8 @@ def main():
         print("Your answer is: 0")
     elif bool1 and bool2 == False:
         print("Your answer is: 0")
+    elif bool1 or bool2 == False:
+        print("Your answer is: 0")
     else:
-        print("You need to enter either a 1 or a 0!")    
+        print("You need to enter either a 1 or a 0!")
 main()
