dfb0d41f1e8850d994b6858f3169abea04f5ebc4
Schloopy <schloopy@protonmail.ch>
2019-05-31 08:03:49 +0100

    finally fixed it! I needed to use an if statement rather than a while loop

--- not.py
+++ not.py
@@ -3,17 +3,13 @@ def main():
     print("This program emulates the function of a NOT logic gate.")
     print("Input 1 or 0 for the program to do its thing!")
     i = input()
-    while i == "0":
+    if i == "0":
         print("Your answer is: 1")
-        break
-    while i  == "1":
+    elif i  == "1":
         print("Your answer is: 0")
-        break
 
-    if i == "20":
+    elif i != "0" or i != "1":
         print("You must enter 1 or 0!")
         
-    if i == "3":
-        print("You must enter either 1 or 0!")
 
 main()
