b1244082eb8fc7caaaf36431228a2eb41caf22ed
Schloopy <schloopy@protonmail.ch>
2019-05-31 11:42:10 +0100

    basically fixed it

--- and.py
+++ and.py
@@ -8,10 +8,14 @@ def main():
         bool1 = False
     elif x == "1":
         bool1 = True
+    else:
+        bool1 = "empty"
     if y == "1":
         bool2 = True
     elif y == "0":
         bool2 = False
+    else:
+        bool2 = "empty"
     
     if bool2 and bool1 == True:
         print("Your answer is: 1")
@@ -21,5 +25,6 @@ def main():
         print("Your answer is: 0")
     elif bool1 or bool2 == False:
         print("Your answer is: 0")
-
+    if bool1 and bool2 == "empty":
+        print("your answer isnt 0, you need to enter either 0 or 1!")
 main()
