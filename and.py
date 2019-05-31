80e363e0a74880aaf2c0476f39ce9d53a54b0ae4
Schloopy <schloopy@protonmail.ch>
2019-05-31 11:35:45 +0100

    its not really fixed, but I made it an end-user problem for now

--- and.py
+++ and.py
@@ -1,20 +1,17 @@
 def main():
     print("This program emulates the functionality of an AND logic gate")
     print("Enter either a 1 or a 0, twice")
+    print("Don't enter anything other than 1 or 0")
     x = input()
     y = input()
     if x == "0":
         bool1 = False
     elif x == "1":
         bool1 = True
-    else:
-        x = None
     if y == "1":
         bool2 = True
     elif y == "0":
         bool2 = False
-    else:
-        y = None
     
     if bool2 and bool1 == True:
         print("Your answer is: 1")
@@ -24,6 +21,5 @@ def main():
         print("Your answer is: 0")
     elif bool1 or bool2 == False:
         print("Your answer is: 0")
-    else:
-        print("You need to enter either a 1 or a 0!")
+
 main()
