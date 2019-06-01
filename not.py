3aabda674fd5912eaff98130caee59c4162b1da9
Schloopy <schloopy@protonmail.ch>
2019-06-01 12:48:56 +0100

    made a repeater function in NOT, so that if you enter anything other than 1 or 0, it tells you off but also starts again

--- not.py
+++ not.py
@@ -1,15 +1,23 @@
-def main():
-    
-    print("This program emulates the function of a NOT logic gate.")
+def intro():
+    print("This program emulates the functionality of a NOT gate")
     print("Input 1 or 0 for the program to do its thing!")
+   
+ 
+def repeat(i):
+    if i != "1" or i != "0":
+        print("You need to enter either 1 or 0!")
+        main()
+
+def main():
+    intro()
     i = input()
     if i == "0":
         print("Your answer is: 1")
     elif i  == "1":
         print("Your answer is: 0")
 
-    elif i != "0" or i != "1":
-        print("You must enter 1 or 0!")
-        
+    else:
+        repeat(i)
+
 
 main()
